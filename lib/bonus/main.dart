017c44ff0afda51a0ed1de5c575f9a6ec4d6df19
Visnuka <sophavisnukakhun@gmail.com>
2025-11-26 23:53:51 +0700

    Done

---
+++ lib/bonus/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'ui/screens/welcome_screen.dart';
+import 'ui/screens/temperature_screen.dart';
+
+
+class TemperatureApp extends StatefulWidget {
+  const TemperatureApp({super.key});
+
+  @override
+  State<TemperatureApp> createState() {
+    return _TemperatureAppState();
+  }
+}
+
+class _TemperatureAppState extends State<TemperatureApp> {
+
+  bool isShowTemperatureScreen = false;
+
+  void switchScreen () {
+    setState(() {
+      isShowTemperatureScreen = !isShowTemperatureScreen;
+    });
+  }
+  
+  @override
+  Widget build(context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xff16C062),
+                Color(0xff00BCDC),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: isShowTemperatureScreen ? TemperatureScreen() : WelcomeScreen(onStart: switchScreen),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const TemperatureApp());
+}
---
+++ lib/bonus/ui/screens/temperature_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class TemperatureScreen extends StatefulWidget {
+  const TemperatureScreen({super.key});
+
+  @override
+  State<TemperatureScreen> createState() => _TemperatureScreenState();
+}
+
+class _TemperatureScreenState extends State<TemperatureScreen> {
+
+  String input = '';
+  double fahrenheit = 0;
+
+  final InputDecoration inputDecoration = InputDecoration(
+    enabledBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.white, width: 1.0),
+      borderRadius: BorderRadius.circular(12),
+    ),
+    hintText: 'Enter a temperature',
+    hintStyle: const TextStyle(color: Colors.white),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Icon(
+              Icons.thermostat_outlined,
+              size: 120,
+              color: Colors.white,
+            ),
+            const Center(
+              child: Text(
+                "Converter",
+                style: TextStyle(color: Colors.white, fontSize: 45),
+              ),
+            ),
+            const SizedBox(height: 50),
+            const Text("Temperature in Degrees:"),
+            const SizedBox(height: 10),
+            TextField(
+              decoration: inputDecoration,
+              keyboardType: TextInputType.number,
+              onChanged: (value) => {
+                setState(() {
+                  input = value;
+                  if (input.isEmpty) {
+                    fahrenheit = 0;
+                  } else {
+                    double celsius = double.tryParse(value) ?? 0;
+                    fahrenheit = celsius * 1.8 + 32;
+                  }
+                })
+              },
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 30),
+            const Text("Temperature in Fahrenheit:"),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Text(
+                fahrenheit.toStringAsFixed(2),
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
